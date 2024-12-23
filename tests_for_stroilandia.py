4409da856bbc3851461e9737f5bff4fe00d50efd
nadezhda.demina <N.Demina@transtelematica.ru>
2024-12-23 23:58:42 +0400

    доработаны тесты

--- tests_for_stroilandia.py
+++ tests_for_stroilandia.py
@@ -1,8 +1,11 @@
+import pytest
+import time
 
 
-
-def test_go_to_store_page(browser, logger):
-    store_name = browser.find_element("xpath", '//div[@class="container-fluid header-v2 logo-row"]//a[@class="header-v2-logo"]')
+@pytest.mark.tryfirst
+def test_go_to_store_page(browser, add_cookie_and_city, logger):
+    store_name = browser.find_element("xpath",
+                                      '//div[@class="container-fluid header-v2 logo-row"]//a[@class="header-v2-logo"]')
     catalog = browser.find_element("xpath", '//div[@class="header-v2-catalog-button"]')
     search_bar = browser.find_element("xpath", '//div[@class="search-input-div"]//input[@class="search-input"]')
 
@@ -10,52 +13,39 @@ def test_go_to_store_page(browser, logger):
     logger.info(message)
 
 
-def test_add_product_to_the_cart(browser, logger):
-    city = browser.find_element("xpath", '//div[@class="cities-locate__buttons"]//a[text()="Да"]')
-    city.click()
-
-    accept_cookies = browser.find_element("xpath", '//div[@class="cookie-block"]//button[@data-role="cooke-accept-btn"]')
-    accept_cookies.click()
-
-    button_catalog = browser.find_element("xpath", '//div[@class="small-banners-block"]//div[@data-creative="Новогодние куклы и фигурки |№1"]')
+@pytest.mark.xfail
+def test_add_product_to_the_cart(browser, add_cookie_and_city, logger):
+    time.sleep(10)
+    button_catalog = browser.find_element("xpath",
+                                          '//a[@href="/catalog/elektroinstrument/?SORT=discount_size"]//child::*')
     button_catalog.click()
-    product = browser.find_element("xpath", '//div[@data-name="Фигурка новогодняя Снеговик в ушанке 47-71 см"]//div[@data-role="basket-add"]')
-    product.click()
+
+    value_in_catalog = browser.find_element("xpath", '//a[@href="/catalog/dreli-shurupoverty/"]//child::*')
+    value_in_catalog.click()
+
+    add_produckt = browser.find_element("xpath", '//div[@data-item="15576845"]//div[@data-role="basket-add"]')
+    add_produckt.click()
 
     message = "товар успешно добавлен в корзину"
-
     logger.info(message)
 
 
-def test_go_to_basket(browser, logger):
-    city = browser.find_element("xpath", '//div[@class="cities-locate__buttons"]//a[text()="Да"]')
-    city.click()
-    accept_cookies = browser.find_element("xpath", '//div[@class="cookie-block"]//button[@data-role="cooke-accept-btn"]')
-    accept_cookies.click()
-
-    basket = browser.find_element("xpath", '//div[@class="container-fluid header-v2 logo-row"]//span[text()="Корзина"]')
+@pytest.mark.trylast
+def test_go_to_basket(browser, add_cookie_and_city, add_to_basket, logger):
+    basket = browser.find_element("xpath", '*//a[@href="/cart/"]')
     basket.click()
 
-    product = browser.find_element("xpath", '//div[@class="basket-item__content"]//a[text()="Фигурка новогодняя Снеговик в ушанке 47-71 см"]')
-    #todo выбрать другой продукт
-    product_price = browser.find_element("xpath", '//div[@class="basket-price__current-price"]//span[text()="1 090 ₽"]')
-    price = int(product_price.get_attribute())
+    product_price = browser.find_element("xpath", '//div[@class="basket-price__current-price"]//span')
+    price = int(product_price.text.replace(' ₽', '').replace(' ', ''))
 
     button_add = browser.find_element("xpath", '//div[@class="counter"]//a[@class="counter__plus"]')
 
     for i in range(2):
         button_add.click()
-        message = 'добавил 2 единицы тoвaра'
+        message = 'добавил 2 единицы товара'
         logger.info(message)
 
-    total_price = browser.find_element("xpath", '//div[@class="total-row total-row--total">3 270 ₽]')
-    value_for_total_price = int(total_price.get_attribute())
+    total_price = browser.find_element("xpath",'//div[@class="total-row total-row--total"]//div[@class="total-row__value"]')
+    value_for_total_price = int(total_price.text.replace(' ₽', '').replace(' ', ''))
 
-    assert price < value_for_total_price
-
-
-
-
-
-
-
+    assert price * 3 == value_for_total_price
